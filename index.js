0358c3a3a797b444bb120690caee41dd638c3ed4
EryetChen <48248414+eryet@users.noreply.github.com>
2020-12-22 23:41:39 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import NfcAppIndexV2 from 'react-native-nfc-manager/example/AppIndexV2';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
